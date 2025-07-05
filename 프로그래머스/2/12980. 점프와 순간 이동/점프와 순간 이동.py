ea91cd24c6ab864ce54c5b19a4479df240b715c1
astra.ka(가을) <74575977+kaeul1020@users.noreply.github.com>
2025-07-05 15:44:30 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    battery = 0
+    
+    while n > 0:
+        if n % 2 == 0:
+            n//=2
+        else:
+            battery +=1
+            n-=1
+        
+    return battery
